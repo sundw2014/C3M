10644adf75b44fd911f47ab34849fe4d6ac2294b
sundw2014 <sundw2014@gmail.com>
2020-11-03 00:54:40 -0600

    add requirements.txt; remove dead code

--- utils.py
+++ utils.py
@@ -1,31 +1,4 @@
-import gzip
-import pickle
-import contextlib
 import numpy as np
-
-def savepklz(data_to_dump, dump_file_full_name):
-    ''' Saves a pickle object and gzip it '''
-
-    with gzip.open(dump_file_full_name, 'wb') as out_file:
-        pickle.dump(data_to_dump, out_file)
-
-
-def loadpklz(dump_file_full_name):
-    ''' Loads a gziped pickle object '''
-
-    with gzip.open(dump_file_full_name, 'rb') as in_file:
-        dump_data = pickle.load(in_file)
-
-    return dump_data
-
-@contextlib.contextmanager
-def temp_seed(seed):
-    state = np.random.get_state()
-    np.random.seed(seed)
-    try:
-        yield
-    finally:
-        np.random.set_state(state)
 
 def EulerIntegrate(controller, f, B, xstar, ustar, xinit, t_max = 10, dt = 0.05, with_tracking = False, sigma = 0., noise_bound = None):
     t = np.arange(0, t_max, dt)
