4e989bee095ff7a5d682bd37533036941a53a521
sundw2014 <sundw2014@gmail.com>
2020-11-03 00:51:36 -0600

    update

--- configs/config_QUADROTOR_8D.py
+++ configs/config_QUADROTOR_8D.py
@@ -12,7 +12,7 @@ omega_y_bound = np.pi / 3
 az_l = -5.
 az_u = 5.
 
-UREF_MIN = np.array([az_l, -omega_x_bound, -omega_y_bound ]).reshape(-1,1)
+UREF_MIN = np.array([az_l, -omega_x_bound, -omega_y_bound]).reshape(-1,1)
 UREF_MAX = np.array([az_u,  omega_x_bound,  omega_y_bound]).reshape(-1,1)
 
 lim = 1.
@@ -20,15 +20,12 @@ XE_MIN = np.array([-lim, -lim, -lim, -lim, -lim, -lim, -lim, -lim]).reshape(-1,1
 XE_MAX = np.array([lim, lim, lim, lim, lim, lim, lim, lim]).reshape(-1,1)
 
 # for sampling ref
-X_INIT_MIN = np.array([-2, -2, -2, -1, -1, -1, 0, 0])
-X_INIT_MAX = np.array([ 2,  2,  2,  1,  1, -1, 0, 0])
+X_INIT_MIN = np.array([-2., -2, -2, -1, -1, -1, 0, 0])
+X_INIT_MAX = np.array([ 2.,  2,  2,  1,  1, -1, 0, 0])
 
 
-XE_INIT_MIN = np.array([-0.5,]*10)
-XE_INIT_MAX = np.array([ 0.5,]*10)
-
-# XE_INIT_MIN = np.array([-1,-1,-1, 0,0,0,0,0,0,0])
-# XE_INIT_MAX = np.array([1,1,1,    0,0,0,0,0,0,0])
+XE_INIT_MIN = np.array([-0.5,]*8)
+XE_INIT_MAX = np.array([ 0.5,]*8)
 
 time_bound = 6.
 time_step = 0.03
@@ -48,9 +45,9 @@ def system_reset(seed):
         weights = (2. * weights / np.sqrt((weights**2).sum(axis=0, keepdims=True))).tolist()
         uref = []
         for _t in t:
-            u = np.array([0, 0, 0]) # ref
+            u = np.array([0., 0, 0]) # ref
             for freq, weight in zip(freqs, weights):
-                u += np.array([weight[0] * np.sin(freq * _t/time_bound * 2*np.pi), 0, 0])
+                u += np.array([weight[0] * np.sin(freq * _t/time_bound * 2*np.pi), 0.1*weight[1] * np.sin(freq * _t/time_bound * 2*np.pi), 0.1*weight[2] * np.sin(freq * _t/time_bound * 2*np.pi)])
             # u += 0.01*np.random.randn(2)
             uref.append(u)
 
